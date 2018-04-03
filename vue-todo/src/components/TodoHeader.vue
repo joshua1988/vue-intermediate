819c99c91de71b381820937d7063510f09d3144f
joshua1988 <jangkeehyo@gmail.com>
2018-04-03 14:30:03 +0900

    Implemented the CRUD features

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
