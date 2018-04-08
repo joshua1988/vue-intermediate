f30be7505e7a3f813d8ad672fbcb866fb5c1b5fb
joshua1988 <jangkeehyo@gmail.com>
2018-04-08 17:27:59 +0900

    Created a vuex store to apply the state and getters in the TodoList component

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in propsdata" class="shadow" v-bind:key="todoItem.item">
+      <li v-for="(todoItem, index) in this.storedTodoItems" class="shadow" v-bind:key="todoItem.item">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
@@ -14,13 +14,18 @@
 
 <script>
 export default {
-  props: ['propsdata'],
   methods: {
     removeTodo(todoItem, index) {
       this.$emit('removeItem', todoItem, index);
     },
     toggleComplete(todoItem, index) {
-      this.$emit('toggleItem', todoItem, index); 
+      this.$emit('toggleItem', todoItem, index);
+    }
+  },
+  computed: {
+    storedTodoItems() {
+      // return this.$store.state.todoItems;
+      return this.$store.getters.getTodoItems;
     }
   }
 }
