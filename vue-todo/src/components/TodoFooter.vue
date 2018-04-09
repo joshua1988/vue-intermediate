c1e00949e53231a9bf10e1a530d9355002108d9a
joshua1988 <jangkeehyo@gmail.com>
2018-04-09 11:33:10 +0900

    Added mapGetters and mapMutations to components

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -5,11 +5,13 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   methods: {
-    clearTodo() {
-      this.$store.commit('clearAllItems');
-    }
+    ...mapMutations({
+      clearTodo: 'clearAllItems'
+    })
   }
 }
 </script>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -2,9 +2,9 @@
   <section>
     <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in this.storedTodoItems" class="shadow" v-bind:key="todoItem.item">
-        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete({todoItem, index})"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span class="removeBtn" v-on:click="removeTodo({todoItem, index})">
           <i class="removeBtn fas fa-trash-alt"></i>
         </span>
       </li>
@@ -13,20 +13,19 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+
 export default {
   methods: {
-    removeTodo(todoItem, index) {
-      this.$store.commit('removeOneItem', {todoItem, index});
-    },
-    toggleComplete(todoItem, index) {
-      this.$store.commit('toggleOneItem', {todoItem, index});
-    }
+    ...mapMutations({
+      removeTodo: 'removeOneItem',
+      toggleComplete: 'toggleOneItem'
+    })
   },
   computed: {
-    storedTodoItems() {
-      // return this.$store.state.todoItems;
-      return this.$store.getters.getTodoItems;
-    }
+    ...mapGetters({
+      storedTodoItems: 'getTodoItems'
+    })
   }
 }
 </script>
