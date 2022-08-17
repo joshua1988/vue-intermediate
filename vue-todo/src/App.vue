f3bca77920a2ca777115ea8296f5753f64172e04
AWOS <71562490+A-WOS@users.noreply.github.com>
2022-08-17 18:58:47 +0900

    Initial commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoIInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {  
+ components: {
+   //컴포넌트 태그명: 컴포넌트 내용
+   'TodoHeader': TodoHeader,
+   'TodoInput': TodoInput,
+   'TodoList': TodoList,
+   'TodoFooter': TodoFooter
+ } 
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="clearAllContainer">
+		<span class="clearAllBtn">Clear All</span>
+	</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.clearAllContainer {
+	width: 8.5rem;
+	height: 50px;
+	line-height: 50px;
+	background-color: white;
+	border-radius: 5px;
+	margin: 0 auto;
+}
+.clearAllBtn {
+	color: #e20303;
+	display: block;
+}
+</style>
